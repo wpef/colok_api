948ffde386ef5000e154964a72216f7cfc6e1fb2
Foulques <Foulques@MacBook-Pro-de-BlaiseWizzMedia.local>
2018-04-22 23:51:41 +0200

    started user handling

---
+++ models/colok.js
@@ -0,0 +1,23 @@
+var mongoose	= require('mongoose');
+var Schema 		= mongoose.Schema;
+var DebtSchema	= require('./debt');
+var PaymentSchema = require('./payment');
+
+var ColokSchema   = new Schema({
+    name	: { type : String, unique : true, }
+    debts	: [DebtSchema],
+    payments : [PaymentSchema],
+    paid	: { type : Boolean, default : false }
+});
+
+ColokSchema.methods.getDebtsTotal = function() {
+	var total = 0;
+	
+	debts.forEach( function (d) {
+		total = total + d.price;
+	});
+
+	return total;
+};
+
+module.exports = mongoose.model('Colok', ColokSchema);
