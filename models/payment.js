223edbd8469bb8765a2a3d1a4e48fe4655a03b48
Foulques <Foulques@MacBook-Pro-de-BlaiseWizzMedia.local>
2018-04-22 22:56:49 +0200

    DB OK + handling debt

--- models/payment.js
+++ models/payment.js
@@ -6,6 +6,15 @@ var PaymentSchema   = new Schema({
     price	: Number,
     owner	: String,
     sharers	: [String],
+    paid	: Boolean, 
 });
+
+PaymentSchema.virtual('n').get(function () {
+  return this.sharers.length;
+});
+
+PaymentSchema.methods.myfunction= function(param) {
+ 		//anything
+  };
 
 module.exports = mongoose.model('Payment', PaymentSchema);
--- server.js
+++ server.js
@@ -33,7 +33,6 @@ var router = express.Router();              // get an instance of the express Ro
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	console.log(mongoose.connection);
 	next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -45,7 +44,15 @@ router.get('/', function(req, res) {
 
 router.route('/payment')
 
+	//get all payments
+	.get(function(req,res) {
+		Payment.find(function(err, payments) {
+			if (err) res.send(err);
+			res.json(payments);
+		});
 
+	})
+	
 	// create a payment (accessed at POST http://localhost:8080/api/payment)
 	.post(function(req, res) {
 
@@ -54,6 +61,7 @@ router.route('/payment')
 		payment.price	= req.body.price;
 		payment.owner	= req.body.owner;
 		payment.sharers	= req.body.sharers;
+		payment.paid 	= false;
 
 
 		// save the payment and check for errors
@@ -66,6 +74,108 @@ router.route('/payment')
 
 	});
 
+router.route('/payment/:payment_id')
+
+    // get the payment by id (accessed at GET http://localhost:8080/api/payment/:payment_id)
+    .get(function(req, res) {
+        Payment.findById(req.params.payment_id, function(err, payment) {
+            if (err) res.send(err);
+            res.json({ payment : payment , effectif : payment.n });
+        });
+    })
+
+    .put(function(req, res){
+		Payment.findById(req.params.payment_id, function(err, payment) {
+            if (err) res.send(err);
+
+            var body = req.body;
+
+            payment.name = body.name ? body.name : payment.name;
+            payment.price = body.price ? body.price : payment.price;
+            payment.owner = body.owner ? body.owner : payment.owner;
+            payment.sharers = body.sharers ? body.sharers : payment.sharers;
+
+            payment.save(function (err) {
+            	if (err) res.send(err);
+
+            	res.json({
+            		payment_id: payment._id,
+            		payment_name : payment.name,
+            		message: 'Payment sucessfully updated!' });
+
+            });
+        });
+	})
+
+	.delete(function(req, res) {
+        Payment.remove({
+            _id: req.params.payment_id
+        }, function(err, payment) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Payment sucessfully deleted!' });
+        });
+    });
+
+//TRYYYYY
+
+router.route('/payment/:payment_id/calc')
+
+    // get the payment by id (accessed at GET http://localhost:8080/api/payment/:payment_id)
+    .get(function(req, res) {
+
+        Payment.findById(req.params.payment_id, function(err, payment) {
+            if (err) res.send(err);
+
+            var total = payment.price;
+            var shared = total / payment.n;
+
+			var debt = [];
+
+            payment.sharers.forEach(function (s) {
+
+            	if (s == payment.owner) {
+            		total = total - shared; 
+            	}
+            	else {
+
+            		total = total - shared; 
+
+            		debt.push({
+            			debt_owner	: s,
+            			debt_price	: shared,
+            			debt_to		: payment.owner,
+            		});
+            	}
+            
+            });
+
+            res.json({ total : payment.price, "Dettes" : debt, reste : total });
+        });
+    
+    })
+
+
+router.route('/payment/:payment_id/pay') 
+
+	.put(function(req, res){
+		Payment.findById(req.params.payment_id, function(err, payment) {
+            if (err) res.send(err);
+            
+            payment.paid = true;
+
+            payment.save(function (err) {
+            	if (err) res.send(err);
+
+            	res.json({
+            		payment_id: payment._id,
+            		payment_name : payment.name,
+            		message: 'Payment sucessfully paid!' });
+
+            });
+        });
+	});
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
