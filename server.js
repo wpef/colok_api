22e0a866fb917f4d2f22c89ed6ac7000990bfcb5
Foulques <f.demoncade@gmail.com>
2018-06-09 16:42:22 +0200

    nicer error message handling

--- server.js
+++ server.js
@@ -60,12 +60,7 @@ app.use((req, res, next) => {
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-      error : error
-    }
-  });
+  res.json({error});
 });
 
 // START THE SERVER
