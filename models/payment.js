725fd29d6a1ae2fe6731816e1b03fa85aeab0679
Foulques <f.demoncade@gmail.com>
2018-06-29 01:00:11 +0200

    handled rest round

--- models/payment.js
+++ models/payment.js
@@ -1,51 +1,49 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var Debt = require('./debt.js');
+var Debt = require("./debt.js");
 
 var PaymentSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, min: 0, required: true },
-  owner: { type: Schema.Types.ObjectId, ref: 'Colok', required: true },
-  sharers: [{ type: Schema.Types.ObjectId, ref: 'Colok', required: true }],
+  owner: { type: Schema.Types.ObjectId, ref: "Colok", required: true },
+  sharers: [{ type: Schema.Types.ObjectId, ref: "Colok", required: true }],
   paid: { type: Boolean, default: false },
   debt_added: { type: Boolean, default: false }
 });
 
-PaymentSchema.virtual('n').get(function() {
+PaymentSchema.virtual("n").get(function() {
   return this.sharers.length;
 });
 
 PaymentSchema.methods.calc_debts = function() {
-  
-  let total = this.price;
-  let reste = total % this.sharers.length;
-  let toShare = total - reste;
-  
-  let shared = toShare / this.sharers.length; 
-  var debts = [];
+
+  let t = Number(this.price);
+  let n = this.sharers.length;
+  let r = Math.round((t % n ) * 100) / 100;
+
+  let round = t / n;
+  let shares = Math.round(round * 100) / 100;
+
+  const debts = [];
 
   this.sharers.forEach(function(s) {
-    total = total - shared;
 
     if (String(s) !== String(this.owner)) {
-
       debts.push(
-
         new Debt({
           from: s,
-          price: shared,
+          price: shares,
           to: this.owner
         })
-
       );
     }
   }, this);
 
-  return { count: debts.length, debts, reste: total };
+  return { count: debts.length, debts, reste: r, payment : this };
 };
 
 PaymentSchema.methods.myfunction = function(param) {
   //anything
 };
 
-module.exports = mongoose.model('Payment', PaymentSchema);
+module.exports = mongoose.model("Payment", PaymentSchema);
